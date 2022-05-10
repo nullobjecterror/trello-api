d0707307de2b9d0321b78560a6cbfc35020173d7
Artyom Tarasov <tarasovar457@gmail.com>
2022-05-10 21:36:42 +0600

    fix: typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -13,29 +13,10 @@ import { CommentsModule } from './comments/comments.module';
     ConfigModule.forRoot({
       validationSchema: Joi.object({
         API_PORT: Joi.number().required(),
-        CONNECTION: Joi.string().required(),
-        USERNAME: Joi.string().required(),
-        PASSWORD: Joi.string().required(),
-        DATABASE: Joi.string().required(),
-        PORT: Joi.number().required(),
         JWT_SECRET: Joi.string().required(),
       }),
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        type: config.get<'aurora-data-api'>('CONNECTION'),
-        username: config.get<string>('USERNAME'),
-        password: config.get<string>('PASSWORD'),
-        database: config.get<string>('DATABASE'),
-        port: config.get<number>('PORT'),
-        entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
-        synchronize: true,
-        autoLoadEntities: true,
-        logging: true,
-      }),
-    }),
+    TypeOrmModule.forRoot(),
     AuthModule,
     UsersModule,
     ColumnsModule,
